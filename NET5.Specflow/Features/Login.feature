c65c264cae89dc6fcb473c968620d1830be2d487
Eduardo Flores <@eduflornet>
2021-07-26 14:46:05 +0200

    Added the implementation of a Login.feature for TestProject with C #, OpenSDK, Selenium and Specflow integration

---
+++ NET5.Specflow/Features/Login.feature
@@ -0,0 +1,14 @@
+﻿Feature: Login
+	Login to Demo application
+
+@smoke
+Scenario: Perform Login of Application site
+	#steps
+	Given I launch the application
+	And I click login link
+	And I enter the following details
+	| UserName | Password |
+	| admin | password |
+	And I click login button
+	Then I should see Employee details link
+
---
+++ NET5.Specflow/Hooks/Hook1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+using TestProject.OpenSDK.Drivers.Web;
+
+namespace NET5.Specflow.Hooks
+{
+    [Binding]
+    public sealed class Hook1
+    {
+        private readonly DriverHelper _driverHelper;
+
+        public Hook1(DriverHelper driverHelper)
+        {
+            _driverHelper = driverHelper;
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            //new DriverManager().SetUpDriver(new ChromeConfig());
+
+
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+
+            Console.WriteLine("Setup");
+
+            //Using TestProject OpenSDK replacing the existing WebDriverManager
+            //Note: Here the Token is taken from the .runsettings file
+            _driverHelper.Driver = new FirefoxDriver(null, "U--LKi5tve35kNidu4uRp5IzAWrhL7PNVp3jEn4Y55U1");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            _driverHelper.Driver.Quit();
+        }
+    }
+}
